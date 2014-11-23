6890e13fe627211b7ebc06aa461757ab35cfa5b3
zoninoz <zoninoz@inventati.org>
2014-11-23 18:20:41 +0100

    problema nomi dei file con Wget corretto

--- libs/downloader_manager.sh
+++ libs/downloader_manager.sh
@@ -125,7 +125,12 @@ function download {
 	if [ ! -z "${post_data}" ]; then
 	    method_post="--post-data=${post_data}"
 	fi
-	[ "$file_in" != "" ] && argout="--trust-server-names" #argout="-O" && fileout="$file_in"
+	if [ "$file_in" != "" ]; then
+	    argout="-O"
+	    fileout="$file_in"
+	else
+	    argout="--trust-server-names"
+	fi
 	wget -t 1 -T $max_waiting --no-check-certificate --retry-connrefused -c -nc --load-cookies=$COOKIES "$method_post" "$url_in_file" -S  $argout "$fileout" -a "$path_tmp/${file_in}_stdout.tmp" & 
 	pid_in=$!
 	echo -e "${pid_in}\nlink_${prog}: $url_in\nWget\n${pid_prog}\nlength_in=$length_in" > "$path_tmp/${file_in}_stdout.tmp"
