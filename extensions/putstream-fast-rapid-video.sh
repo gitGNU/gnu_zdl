fa29de1cd4048c6ac7130af59251d686f2ce318c
zoninoz <zoninoz@inventati.org>
2015-01-22 01:05:06 +0100

    nuova estensione per putstream

--- extensions/putstream-fast-rapid-video.sh
+++ extensions/putstream-fast-rapid-video.sh
@@ -26,26 +26,30 @@
 #
 
 ## zdl-extension types: streaming
-## zdl-extension name: Fastvideo, Rapidvideo
+## zdl-extension name: Fastvideo, Rapidvideo, Putstream
 
 
-if [[ "$url_in" =~ (fastvideo.|rapidvideo.) ]] && [[ ! "$url_in" =~ embed ]]; then
-    link_parser "$url_in"
-    links_loop - "$url_in"
-    parser_path="${parser_path%%\/*}"
-    url_in="${parser_proto}${parser_domain}/embed-${parser_path}-607x360.html"
-    links_loop + "$url_in"
-fi
+if [[ "$url_in" =~ (fastvideo.|rapidvideo.|putstream.) ]]; then
+    if [[ ! "$url_in" =~ embed ]]; then
+	if [[ "$url_in" =~ (putstream.) ]]; then
+	    html=$(wget -q -O- "$url_in")
+	    file_in=$(grep '<Title>' <<< "$html" |sed -r 's|.*<Title>([^<>]+)<.+|\1|g')
+	fi
+	link_parser "$url_in"
+	parser_path="${parser_path%%\/*}"
+	url_packed="${parser_proto}${parser_domain}/embed-${parser_path%.html}-607x360.html"
+    fi
 
-if [[ "$url_in" =~ (fastvideo.|rapidvideo.) ]]; then
-    html_packed=$(wget "$url_in" -O- -q |grep 'p,a,c,k,e,d')
+    html_packed=$(wget "$url_packed" -O- -q |grep 'p,a,c,k,e,d')
     if [ ! -z "$html_packed" ]; then
 	packed_args "$html_packed"
 	packed_code=$(packed "$code_p" "$code_a" "$code_c" "$code_k")
 	url_in_file=$(sed -r 's@.+file\:\"http([^"]+)mp4\".+@http\1mp4@' <<< "$packed_code")
 
+	if [[ ! "$url_in" =~ (putstream.) ]]; then
 	#file_in=$(grep '<title>' < "$1" |tail -n1 |sed -r 's@.*>([^<>]+)<.*@\1@g')."${url_in_file##*.}"
-	file_in=$(sed -r 's@.+tv_file_name\=\"([^"]+)\".+@\1@' <<< "$packed_code")
+	    file_in=$(sed -r 's@.+tv_file_name\=\"([^"]+)\".+@\1@' <<< "$packed_code")
+	fi
 	axel_parts=4
     else
 	break_loop=true
