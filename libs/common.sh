836138dab13c4b272847c6f0a8e69409eeeeeebf
zoninoz <zoninoz@inventati.org>
2015-03-21 17:36:12 +0100

    sanitizing file_in

--- libs/common.sh
+++ libs/common.sh
@@ -89,10 +89,12 @@ function is_rtmp {
 function sanitize_file_in {
     file_in="${file_in// /_}"
     file_in="${file_in//\'/_}"
-    file_in="${file_in//[\[\]\(\)]}"
+    file_in="${file_in//[\[\]\(\)]/-}"
     file_in="${file_in##*/}"
     file_in="$(htmldecode "$file_in")"
-    file_in="${file_in//&/and}"
+    file_in="${file_in//'&'/and}"
+    file_in="${file_in//'#'}"
+    file_in="${file_in//';'}"
     file_in="${file_in::240}"
     file_in=$(sed -r 's|^[^0-9a-zA-Z\[\]()]*([0-9a-zA-Z\[\]()]+)[^0-9a-zA-Z\[\]()]*$|\1|g' <<< "$file_in")
 }
