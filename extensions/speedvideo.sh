702cfc3ea54330dfecf897fcf05e02f5ddd1b6c8
zoninoz <zoninoz@inventati.org>
2015-07-11 00:32:05 +0200

    speedvideo

--- extensions/speedvideo.sh
+++ extensions/speedvideo.sh
@@ -41,6 +41,8 @@ if [[ "$url_in" =~ (speedvideo.) ]]; then
 	var2=$(grep base64_decode <<< "$html" |sed -r 's|.+ ([^ ]+)\)\;$|\1|g')
 	url_in_file=$(base64_decode $linkfile $(grep "$var2" <<< "$html" |head -n1 |sed -r 's|.+ ([^ ]+)\;$|\1|g') )
 	file_in=$(wget -q -O- "$url_in" |grep 'itle>' |sed -r 's|.*itle>([^<>]+)<.+|\1|g').${url_in_file##*.}
+	file_in="${file_in#Watch }"
+
 	axel_parts=4
 	link_parser "$url_in_file"
 	if [ $? != 1 ] && [ "$file_in" == ".${url_in_file##*.}" ]; then
