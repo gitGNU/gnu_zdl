0ff48093367a3f9a9ec8d74a08721e662a80e8ad
zoninoz <zoninoz@inventati.org>
2016-05-17 17:07:23 +0200

    eval.js

--- libs/eval.js
+++ libs/eval.js
@@ -1,5 +1,12 @@
 var a = process.argv.slice(2);
 var vm = require('vm');
-var context = vm.createContext({ console: console });
+var context = vm.createContext();
 var script = new vm.Script(a[0]);
 console.log(script.runInNewContext(context));
+
+//////// ALTERNATIVE VERSION:
+//
+// var a = process.argv.slice(2);
+// eval("var unpacked=String" + a[0]);
+// console.log(unpacked);
+
