21dc52f68f58804abecde5a7bc09ffebe569ce20
zoninoz <zoninoz@inventati.org>
2016-05-18 14:43:51 +0200

    backin

---
+++ extensions/backin.sh
@@ -0,0 +1,58 @@
+#!/bin/bash -i
+#
+# ZigzagDownLoader (ZDL)
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/. 
+# 
+# Copyright (C) 2011: Gianluca Zoni (zoninoz) <zoninoz@inventati.org>
+# 
+# For information or to collaborate on the project:
+# https://savannah.nongnu.org/projects/zdl
+# 
+# Gianluca Zoni (author)
+# http://inventati.org/zoninoz
+# zoninoz@inventati.org
+#
+
+## zdl-extension types: streaming
+## zdl-extension name: Backin
+
+if [ "$url_in" != "${url_in//backin.}" ]
+then
+    if [[ ! "$url_in" =~ stream- ]]
+    then
+
+    	link_parser "$url_in"
+    	stream_id="${url_in##*\/}"
+	stream_id="${stream_id##*s=}"
+    	url_stream="${parser_proto}${parser_domain}/stream-${stream_id}-500x400.html"
+	
+    else
+    	url_stream="$url_in"
+    fi
+    
+    html=$(wget "$url_stream" -qO- --user-agent="$user_agent")
+
+    url_in_file=$(grep source <<< "$html" |
+			 sed -r 's|.+src=\"([^"]+)\".+|\1|g')
+    
+    file_in=$(grep '<title>' <<< "$html" |
+		     sed -r 's|[^>]+>([^<]+)<.+|\1|g')
+    file_in="${file_in#Streaming }"
+
+    if ! url "$url_in_file" ||
+	    [ -z "$file_in" ]
+    then
+	_log 2
+    fi
+fi
