5414e5e47664de00cd4f020abf388f2e8261be5a
zoninoz <zoninoz@inventati.org>
2016-05-21 22:28:48 +0200

    aadecoder per cygwin

--- libs/aadecoder.php
+++ libs/aadecoder.php
@@ -1,14 +1,6 @@
 <?php
 
-//$file = $HTTP_GET['file'];
-
-require "php-aaencoder-master/AADecoder.php";
-
-#echo AADecoder::decode(file_get_contents('encoded.js'));
-#echo AADecoder::decode(file_get_contents('../out-videowood'));
-#echo AADecoder::decode(file_get_contents('out-encoded'));
-#echo AADecoder::decode(file_get_contents('coded-openload'));
-#echo AADecoder::decode(file_get_contents('aaencoded.clean'));
+require "$path_usr/libs/php-aaencoder-master/AADecoder.php";
 echo AADecoder::decode(file_get_contents($argv[1]));
 
 ?>
