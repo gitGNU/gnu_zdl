40bc804eaa0e8084b8ad2ae64212585cfb6ea63c
zoninoz <zoninoz@inventati.org>
2016-05-21 23:31:36 +0200

    aadecoder per cygwin

--- libs/aadecoder.php
+++ libs/aadecoder.php
@@ -1,6 +1,6 @@
 <?php
 
-require "$path_usr/libs/php-aaencoder-master/AADecoder.php";
+require "/usr/local/share/zdl/libs/php-aaencoder-master/AADecoder.php";
 echo AADecoder::decode(file_get_contents($argv[1]));
 
 ?>
