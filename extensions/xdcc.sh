2f574a926b0ab00b59f5032958f11692cc461dd5
zoninoz <zoninoz@inventati.org>
2016-08-08 15:00:22 +0200

    xdcc://

--- extensions/xdcc.sh
+++ extensions/xdcc.sh
@@ -26,7 +26,9 @@
 
 if [[ "$url_in" =~ ^xdcc://[^/]+/([^/]+)/([^/]+)/([^/]+)/([^/]+) ]]
 then
-    replace_url_in "$(sanitize_url "irc://${BASH_REMATCH[1]}/${BASH_REMATCH[2]##\#}/msg ${BASH_REMATCH[3]} xdcc send ${BASH_REMATCH[4]}")"
+    irc_chan="${BASH_REMATCH[2]##\#}"
+    irc_chan="${irc_chan##\%23}"
+    replace_url_in "$(sanitize_url "irc://${BASH_REMATCH[1]}/$irc_chan/msg ${BASH_REMATCH[3]} xdcc send ${BASH_REMATCH[4]}")"
 fi
 									   
 if [[ "$url_in" =~ ^irc:\/\/ ]]
