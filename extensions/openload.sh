1603ee2b819dd3787d5283979dca5bddfb5ff67e
zoninoz <zoninoz@inventati.org>
2016-08-15 14:46:08 +0200

    varie

--- extensions/openload.sh
+++ extensions/openload.sh
@@ -55,7 +55,10 @@ then
 
 	hiddenurl=$(htmldecode "$hiddenurl")
 	hiddenurl="${hiddenurl//\'/\\\'}"
+	hiddenurl="${hiddenurl//\"/\\\"}"
 
+	countdown- 6
+	
 	chunk2=$(nodejs -e "var x = '$hiddenurl'; var s=[];for(var i=0;i<x.length;i++){var j=x.charCodeAt(i);if((j>=33)&&(j<=126)){s[i]=String.fromCharCode(33+((j+14)%94));}else{s[i]=String.fromCharCode(j);}}; console.log(s.join(''))")
 
 	if [ -n "$chunk2" ]
