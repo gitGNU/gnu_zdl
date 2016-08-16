fd661ffe88cf121f3bc6c5e8dee3943b21b6ac61
zoninoz <zoninoz@inventati.org>
2016-08-16 04:39:43 +0200

    nodejs

--- extensions/openload.sh
+++ extensions/openload.sh
@@ -60,7 +60,7 @@ then
 
 	countdown- 6
 	
-	chunk2=$(nodejs -e "var x = '$hiddenurl'; var s=[];for(var i=0;i<x.length;i++){var j=x.charCodeAt(i);if((j>=33)&&(j<=126)){s[i]=String.fromCharCode(33+((j+14)%94));}else{s[i]=String.fromCharCode(j);}}; console.log(s.join(''))")
+	chunk2=$($nodejs -e "var x = '$hiddenurl'; var s=[];for(var i=0;i<x.length;i++){var j=x.charCodeAt(i);if((j>=33)&&(j<=126)){s[i]=String.fromCharCode(33+((j+14)%94));}else{s[i]=String.fromCharCode(j);}}; console.log(s.join(''))")
 
 	if [ -n "$chunk2" ]
 	then
