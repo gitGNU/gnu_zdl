c9fc9d07551319df05dbe8a9274d0070b27265c9
zoninoz <zoninoz@inventati.org>
2016-08-18 00:27:21 +0200

    varie

--- extensions/openload.sh
+++ extensions/openload.sh
@@ -57,11 +57,12 @@ then
 	hiddenurl="${hiddenurl//\'/\\\'}"
 	hiddenurl="${hiddenurl//\"/\\\"}"
 	hiddenurl="${hiddenurl//\`/\\\`}"
-
+	hiddenurl="${hiddenurl//\$/\\\$}"
+	
 	countdown- 6
 	
 	chunk2=$($nodejs -e "var x = '$hiddenurl'; var s=[];for(var i=0;i<x.length;i++){var j=x.charCodeAt(i);if((j>=33)&&(j<=126)){s[i]=String.fromCharCode(33+((j+14)%94));}else{s[i]=String.fromCharCode(j);}}; console.log(s.join(''))")
-	
+
 	if [ -n "$chunk2" ]
 	then
 	    url_in_file=$(wget -S --spider "https://openload.co/stream/$chunk2" 2>&1 |
