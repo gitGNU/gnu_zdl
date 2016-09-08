32eb84556f798b59d59be0722175396272df5d2c
zoninoz <zoninoz@inventati.org>
2016-09-08 19:30:54 +0200

    movshare -> wholecloud

--- extensions/movshare.sh
+++ extensions/movshare.sh
@@ -26,61 +26,96 @@
 
 ## ZDL add-on
 ## zdl-extension types: streaming
-## zdl-extension name: Movshare
+## zdl-extension name: Movshare/Wholecloud
 
-if [ "$url_in" != "${url_in//'movshare.'}" ] &&
+if [[ "$url_in" =~ (movshare\.|wholecloud\.) ]] &&
        [ "$url_in" != "${url_in//video}" ]
 then
-    html="$(wget -t 1 -T $max_waiting "$url_in" -O- -q)"
+    replace_url_in "http://www.wholecloud.net/video/${url_in##*\/}"
+    
+    html=$(wget -t 1 -T $max_waiting \
+		--keep-session-cookies \
+		--save-cookies="$path_tmp"/cookies.zdl \
+		"$url_in" -qO- )
 
     if [ -n "$html" ]
     then
 	test_exist=$(grep "This file no longer exists on our servers" <<< "$html")
 	if [ -n "$test_exist" ]
 	then
-	    # not_available=true
-	    # break_loop=true
 	    _log 3
 
 	else
-	    flashvars_file=$(grep "flashvars.file=" <<< "$html")
-	    flashvars_file="${flashvars_file#*'flashvars.file='\"}"
-	    flashvars_file="${flashvars_file%\"*}"
-
-	    flashvars_key=$(grep "flashvars.filekey=" <<< "$html")
-	    flashvars_key="${flashvars_key#*'flashvars.filekey='\"}"
-	    flashvars_key="${flashvars_key%\"*}"
-	    flashvars_domain=$(grep "flashvars.domain=" <<< "$html")
-	    flashvars_domain="${flashvars_domain#*'flashvars.domain='\"}"
-	    flashvars_domain="${flashvars_domain%\"*}"
-
-	    rm -f "$path_tmp"/zdl2.tmp
-	    axel "${flashvars_domain}/api/player.api.php?user=undefined&cid=1&file=${flashvars_file}&pass=undefined&key=${flashvars_key}" -o "$path_tmp"/zdl2.tmp &>/dev/null 
-	    if [ ! -f "$path_tmp"/zdl2.tmp ]
+	    if [[ "$html" =~ (Continue to the video) ]]
 	    then
-		_log 5
+		input_hidden "$html"
+		post_data+="&submit=submit"
 		
-	    elif [ -z $(grep url "$path_tmp"/zdl2.tmp ) ]
-	    then
-		not_available=true
-		break_loop=true
-	    else
-		url_in_file=$(cat "$path_tmp"/zdl2.tmp)
-		url_in_file="${url_in_file#*'url='}"
-		url_in_file="${url_in_file%%'&'*}"
-		file_in="${url_in_file##*'/'}"
-		file_in=$(grep "share" "$path_tmp"/zdl.tmp |grep "title=")
-		file_in="${file_in#*'title='}"
-		file_in="${file_in%%\"*}.${url_in_file##*'.'}"
+		html=$(wget -t 1 -T $max_waiting                     \
+			    "$url_in"                                \
+			    --referer=wholecloud.net                 \
+			    --user-agent="$user_agent"               \
+			    --post-data="${post_data}"               \
+			    --load-cookies="$path_tmp"/cookies.zdl \
+			    -qO-)
 
-		if [[ "$file_in" =~ ^\. ]]
+	    fi
+	    file_in=$(grep "Title:" <<< "$html")
+	    file_in="${file_in%<*}"
+	    file_in=$(trim "${file_in##*>}")
+	    
+	    [ "$file_in" == Untitled ] &&
+		file_in=${file_in}-${url_in##*\/}
+	    
+	    url_in_file=$(grep "source" <<< "$html" |
+				 head -n1 |
+				 sed -r 's|.+\"([^"]+)\".+|\1|g')
+
+	    if ! url "$url_in_file"
+	    then
+		flashvars_file=$(grep "flashvars.file=" <<< "$html")
+		flashvars_file="${flashvars_file#*'flashvars.file='\"}"
+		flashvars_file="${flashvars_file%\"*}"
+
+		flashvars_key=$(grep "flashvars.filekey=" <<< "$html")
+		flashvars_key="${flashvars_key#*'flashvars.filekey='\"}"
+		flashvars_key="${flashvars_key%\"*}"
+		flashvars_domain=$(grep "flashvars.domain=" <<< "$html")
+		flashvars_domain="${flashvars_domain#*'flashvars.domain='\"}"
+		flashvars_domain="${flashvars_domain%\"*}"
+
+		rm -f "$path_tmp"/zdl2.tmp
+		axel "${flashvars_domain}/api/player.api.php?user=undefined&cid=1&file=${flashvars_file}&pass=undefined&key=${flashvars_key}" -o "$path_tmp"/zdl2.tmp &>/dev/null
+						    
+		if [ ! -f "$path_tmp"/zdl2.tmp ]
 		then
-		    url_in="${url_in%%\/}"
-		    file_in="${url_in##*\/}$file_in"
+		    _log 5
+		    
+		elif ! grep url "$path_tmp"/zdl2.tmp &>/dev/null
+		then
+		    not_available=true
+		    break_loop=true
+
+		else
+		    url_in_file=$(cat "$path_tmp"/zdl2.tmp)
+		    url_in_file="${url_in_file#*'url='}"
+		    url_in_file="${url_in_file%%'&'*}"
+
+		    if [ -z "$file_in" ]
+		    then
+			file_in=$(grep "Title:" "$path_tmp"/zdl2.tmp)
+			file_in="${file_in%<*}"
+			file_in=$(trim "${file_in##*>}")
+
+		    
+			[ "$file_in" == Untitled ] &&
+			    file_in=${file_in}-${url_in##*\/}
+		    fi
 		fi
 	    fi
-	fi
-    else
-	_log 2
+	fi	    
     fi
+    try_end=15
+    end_extension
+    
 fi
