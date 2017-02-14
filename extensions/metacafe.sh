2371164c101c9e4ab5ba71079bda1a1c04e35bce
zoninoz <zoninoz@inventati.org>
2017-02-14 19:45:50 +0100

    metacafe

---
+++ extensions/metacafe.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -i
+#
+# ZigzagDownLoader (ZDL)
+# 
+# This program is free software: you can redistribute it and/or modify it 
+# under the terms of the GNU General Public License as published 
+# by the Free Software Foundation; either version 3 of the License, 
+# or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, 
+# but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see http://www.gnu.org/licenses/. 
+# 
+# Copyright (C) 2011: Gianluca Zoni (zoninoz) <zoninoz@inventati.org>
+# 
+# For information or to collaborate on the project:
+# https://savannah.nongnu.org/projects/zdl
+# 
+# Gianluca Zoni (author)
+# http://inventati.org/zoninoz
+# zoninoz@inventati.org
+#
+
+## zdl-extension types: streaming
+## zdl-extension name: Metacafe
+
+if [ "$url_in" != "${url_in//metacafe.}" ]
+then
+    html=$(wget -qO- "$url_in")
+
+    file_in=$(trim "$(grep '<title>' -A2 <<< "$html" | sed -n 2p)")
+    url_in_file=$(grep m3u8 <<< "$html" | head -n1 | tr -d '\\' |
+			 sed -r 's|.+src\":\"([^"]+)\".+|\1|g')
+
+    end_extension
+fi
